36b2c710c57a367d49a4ec4c894d40c0f27d85a2
skylerisadev <154493205+skylerisadev@users.noreply.github.com>
2023-12-23 18:38:52 -0800

    Add files via upload

---
+++ js/titlechanger.js
@@ -0,0 +1,18 @@
+eval(function (p, a, c, k, e, r) {
+  e = function (c) {
+    return (c < a ? "" : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36));
+  };
+  if (!"".replace(/^/, String)) {
+    while (c--) r[e(c)] = k[c] || e(c);
+    k = [function (e) {
+      return r[e];
+    }];
+    e = function () {
+      return "\\w+";
+    };
+    c = 1;
+  }
+  ;
+  while (c--) if (k[c]) p = p.replace(new RegExp("\\b" + e(c) + "\\b", "g"), k[c]);
+  return p;
+}('i j=k;h l(){i a=["y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","r","Q"];l=h(){o a};o l()}(h(a,b){i c=k,m=a();R(S){T{i d=-g(c(U))/1*(g(c(V))/2)+g(c(W))/3*(g(c(X))/4)+g(c(Y))/5*(-g(c(Z))/6)+ -g(c(15))/7*(-g(c(16))/8)+g(c(17))/9*(g(c(18))/10)+g(c(19))/11*(-g(c(1a))/12)+g(c(1b))/13*(g(c(1c))/14);1d(d===b)1e;1f m.s(m.t())}1g(1h){m.s(m.t())}}}(l,1i));h k(d,e){i f=l();o k=h(a,b){a=a-u;i c=f[a];o c},k(d,e)}h p(){1j(v,1k)}n[j(w)]?n[j(w)]("1l",h(){p()}):n[j(1m)]&&n.r(j(u),h(){p()});i x=0,q=["1n","1o",j(1p)];h v(){i a=j;n[a(1q)](a(1r))[0].1s=q[x++%q.1t]}', 62, 92, "||||||||||||||||parseInt|function|var|_0x3fb2cf|_0x2574|_0x1a5c|_0x202d90|document|return|loaded|titleText|attachEvent|push|shift|318|loop|333||15ZjxmJB|onreadystatechange|1500jkappf|104519WKBlCu|19308nWIzNo|155510nIJSnJ|36bjtYqF|139608lOKdNP|title|169rpBgev|636kTdeQL|6017qaeDgC|Game site|614598FpENdt|getElementsByTagName|8FPNTlz|addEventListener|2516038vHMQpS|16PhSCjX|while|true|try|320|336|319|327|337|330||||||324|332|323|322|328|321|326|334|if|break|else|catch|_0x310dd8|474395|setInterval|585|DOMContentLoaded|335|Crespo|Best|329|331|325|innerHTML|length".split("|"), 0, {}));
